743f1b43a8e722d6678bbdf89789c0b6b9504065
timur.karev <timurkarev@mail.ru>
2023-10-25 10:29:16 +0700

    fire chat attempt

--- lib/data/use_case/demonstration/fire_chat_use_case.dart
+++ lib/data/use_case/demonstration/fire_chat_use_case.dart
@@ -3,17 +3,31 @@ import 'package:auth/domain/failers/failer.dart';
 import 'package:auth/domain/use_cases/demonstration/chat_use_case.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:dartz/dartz.dart';
+import 'package:http/http.dart';
 
 class FireChatUseCase implements ChatUseCase {
+  static const _httpFunctionUrl =
+      "https://us-central1-moto-pattaya.cloudfunctions.net/";
+
   @override
   Future<Either<Failure, String>> askQuestion({
     required String question,
     required String collectionName,
   }) async {
+    const functionName = 'ask_question';
+
     try {
       final result =
-          await FirebaseFunctions.instance.httpsCallable('addMessage').call();
-      return Right(result.data.toString());
+          await get(Uri.parse('$_httpFunctionUrl$functionName'), headers: {
+        'question': question,
+        'collection_name': collectionName,
+      });
+      // final result =
+      //     await FirebaseFunctions.instance.httpsCallable('ask_question').call({
+      //   'question': question,
+      //   'collection_name': collectionName,
+      // });
+      return Right(result.body);
     } on FirebaseFunctionsException catch (e) {
       return Left(
         SomeChatFailure(
